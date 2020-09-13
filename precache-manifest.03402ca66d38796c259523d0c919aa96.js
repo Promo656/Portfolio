52efe8a3c8a22dc5bba683444b80291a7c794b8d
Egor <rank2007@mail.ru>
2020-09-13 18:29:30 +0300

    Updates

---
+++ precache-manifest.03402ca66d38796c259523d0c919aa96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60289044face86a1ed6650c794a1f58",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "aa1b8ab05e4ce0d651b4",
+    "url": "/Portfolio/static/css/main.a487eca2.chunk.css"
+  },
+  {
+    "revision": "34c6b124eac91b683950",
+    "url": "/Portfolio/static/js/2.1ab23ced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.1ab23ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1b8ab05e4ce0d651b4",
+    "url": "/Portfolio/static/js/main.461ac3a4.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
